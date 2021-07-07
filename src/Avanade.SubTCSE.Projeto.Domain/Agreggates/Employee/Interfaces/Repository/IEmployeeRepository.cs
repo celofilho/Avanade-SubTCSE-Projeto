1f0ca8f792bc2d14f38f0e351226267b95ffc0ad
Marcelo S. M. Filho <marcelosilvafilho@gmail.com>
2021-07-07 08:04:32 -0300

    criando MongoDBContext

--- src/Avanade.SubTCSE.Projeto.Domain/Agreggates/Employee/Interfaces/Repository/IEmployeeRepository.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Agreggates/Employee/Interfaces/Repository/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Agreggates.Employee.Interfaces.Repository
 {
-    public interface IEmployeeRepository
+    public interface IEmployeeRepository : IBaseRepository<Domain.Agreggates.Employee.Entities.Employee, string>
     {
-
+       
     }
 }
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Agreggates;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Base.Repository
+{
+    public interface IBaseRepository<TEntity, TId>
+        where TEntity : BaseEntity<TId>
+    {
+        Task<TEntity> Add(TEntity entity);
+
+        Task<TEntity> FindById(TId id);
+
+    }
+}
